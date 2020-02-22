3993fa636d9d4bff32d57a7f9b0e3b5f98898458
yasnling001 <1964074623@qq.com>
2020-02-23 07:16:45 +0800

    demo

--- src/main/java/com/example/demo/SchoolApplication.java
+++ src/main/java/com/example/demo/SchoolApplication.java
@@ -3,10 +3,17 @@ package com.example.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
 @MapperScan("com.example.demo.dao")
-public class SchoolApplication {
+public class SchoolApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SchoolApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SchoolApplication.class, args);
